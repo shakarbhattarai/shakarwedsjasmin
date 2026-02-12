6e13f19c8c72b89a6ce9cbbf39c4f8229106ddc9
Shakar Bhattarai <shakarbhattarainp@gmail.com>
2026-02-12 09:51:32 +0545

    Create src/App.jsx with React wedding component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Wedding = () => {
+  return (
+    <div>
+      <h1>Welcome to the Wedding!</h1>
+      <p>Join us in celebrating our special day.</p>
+    </div>
+  );
+};
+
+export default Wedding;
